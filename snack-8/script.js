5124e520480ee44e9a8920a679c21bdc4088d747
marco torremacco <mtorremacco@gmail.comm>
2024-12-17 12:28:15 +0100

    done last snack

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classroomOfId1 = students.find(students => students.id === 1)
+console.log(classroomOfId1.class)
